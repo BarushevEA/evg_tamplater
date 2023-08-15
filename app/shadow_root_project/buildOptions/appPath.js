b8ea3c226b0da4c7ac390f65b359ba245a869f47
Evgeny <b_evgeny@ukr.net>
2023-08-16 00:44:42 +0300

    delete unoptimized project

--- app/shadow_root_project/buildOptions/appPath.js
+++
@@ -1,8 +0,0 @@
-// Build attributes
-exports.buildDirLevel = "../../../";
-exports.buildDirPath = "./builds/shadow_root_project";
-exports.buildFileName = "shadow_root_project.js";
-
-// App style and templates dirs
-exports.HTMLTemplatesDirPath = "../src/templates/";
-exports.CSSDirPath = "../src/css";
--- app/shadow_root_project/src/APP_INFO.ts
+++
@@ -1,3 +0,0 @@
-import {AppInfo} from "../../../libs/utils/appInfo";
-
-export const APP_INFO = new AppInfo('shadow_root_project', 1, 0, 0);
