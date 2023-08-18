e96258701bfe498d9454e9c8f3e8e651ffa9d2f0
Evgeny <b_evgeny@ukr.net>
2023-08-18 04:00:14 +0300

    optimize naming

--- libs/elements/rootElements/managers/APP_TAG_NAME.ts
+++ libs/elements/rootElements/managers/APP_TAG_NAME.ts
@@ -1,3 +1,26 @@
 import {getAppInfo} from "../../../utils/utils";
 
-export const APP_TAG_NAME = (<any>getAppInfo().name).replaceAll("_", "-");
+const name = getAppInfo().name;
+const symbols = "abcdefghijklmnopqrstuvwxyz";
+let appSuffix = "";
+
+for (let i = 0; i < name.length; i++) {
+    const nameSymbol = name[i];
+    let result = ""
+
+    for (let j = 0; j < symbols.length; j++) {
+        const symbol = symbols[j];
+        if (symbol === nameSymbol.toLowerCase()) {
+            result = symbol;
+            break;
+        }
+    }
+
+    if (!result) {
+        appSuffix += "-";
+    } else {
+        appSuffix += result;
+    }
+}
+
+export const APP_TAG_NAME = "app-" + appSuffix;
