8d8a9f0e2bb1304179bc0c51f4e33ea2561b2ec5
Evgeny <b_evgeny@ukr.net>
2023-09-03 03:01:11 +0300

    add css class uglifier

---
+++ app/optimized_project/buildOptions/flags.js
@@ -0,0 +1,3 @@
+exports.flag = {
+    isCssEncrypt: true,
+};
--- app/optimized_project/buildTemplateHandler/cssHandler.js
+++ app/optimized_project/buildTemplateHandler/cssHandler.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const env = require('./utils');
 const options = require('../buildOptions/templateOptions');
+const flags = require('../buildOptions/flags');
 
 const cssPath = env.getCSSPath();
 const buildFilePath = env.getBuildFilePath();
@@ -19,6 +20,13 @@ fs.readFile(cssPath, (error, data) => {
         handleError(error);
 
         let jsFileStr = data.toString();
+
+        if (flags.flag.isCssEncrypt) {
+            const encryptedObj = getEncryptedObject(jsFileStr, cssFileStr);
+            jsFileStr = encryptedObj.js;
+            cssFileStr = encryptedObj.css;
+        }
+
         jsFileStr = jsFileStr.replace(options.CSS_Marker, cssFileStr);
 
         fs.writeFile(buildFilePath, jsFileStr, function (error) {
@@ -26,6 +34,69 @@ fs.readFile(cssPath, (error, data) => {
         });
     });
 });
+
+function getEncryptedObject(jsFileStr, cssFileStr) {
+    let index = 0;
+    const classMarker = `class='`;
+    const classStrList = [];
+    const classStrListModified = [];
+    const classes = [];
+
+    while ((index = jsFileStr.indexOf(classMarker, index)) > -1) {
+        index += classMarker.length;
+        let clsStr = "";
+        let sym = "";
+        while ((sym = jsFileStr[index]) !== "'") {
+            clsStr += sym;
+            index++;
+        }
+
+        if (classStrList.indexOf(clsStr) === -1) {
+            classStrList.push(clsStr);
+            classStrListModified.push(clsStr)
+
+            clsStr.split(" ").forEach(cls => {
+                if (cls && classes.indexOf(cls) === -1) {
+                    classes.push(cls);
+                }
+            });
+        }
+    }
+
+    classes.sort((a, b) => {
+        if (a.length > b.length) return -1;
+        return 1;
+    });
+
+    classes.forEach((cls, index) => {
+        classStrListModified.forEach((clsStr, i) => {
+            classStrListModified[i] = clsStr.replaceAll(cls, "" + getClassName(index));
+        });
+        cssFileStr = cssFileStr.replaceAll(`.${cls} `, `.${getClassName(index)} `);
+    });
+
+    classStrList.forEach((clsStr, index) => {
+        jsFileStr = jsFileStr.replaceAll(`${classMarker}${clsStr}'`, `${classMarker}${classStrListModified[index]}'`);
+    });
+
+    return {
+        js: jsFileStr,
+        css: cssFileStr,
+    };
+}
+
+function getClassName(index) {
+    let cssClass = "";
+    const symbols = "qwertyuiop";
+    const smbIndex = "" + index;
+
+    for (let i = 0; i < smbIndex.length; i++) {
+        const ind = +smbIndex[i];
+        cssClass += symbols[ind];
+    }
+
+    return cssClass;
+}
 
 function handleError(error) {
     if (error) {
