21a9c8426d559ce1beb290833c237f4326d785a8
grif <b_evgeny@ukr.net>
2023-10-02 03:34:38 +0300

    add edit cell behavior

--- app/simple_table/src/modules/elements/cell/cell.ts
+++ app/simple_table/src/modules/elements/cell/cell.ts
@@ -4,17 +4,22 @@ export class Cell implements OnInit, OnCreate, OnDestroy {
     readonly root;
     name: string;
     data: string;
+    input: HTMLElement;
+    isEdit: boolean;
 
     constructor(root: RootBehavior) {
         this.root = root;
         this.name = root.tagName;
         this.data = "test";
+        this.isEdit = false;
     }
 
     onCreate(): void {
         this.root.dataCatch$<string>()
             .subscribe(data => {
                 this.data = data;
+                // @ts-ignore
+                this.input.value = data;
                 this.root.title = data;
                 this.root.detectChanges();
             });
@@ -25,5 +30,30 @@ export class Cell implements OnInit, OnCreate, OnDestroy {
     }
 
     onDestroy(): void {
+    }
+
+    onDblClick(): void {
+        this.isEdit = true;
+        this.root.detectChanges();
+    }
+
+    onKeyDown(evt: KeyboardEvent): void {
+        if (!this.isEdit) return;
+
+        if (evt.key.toLowerCase()==="enter") {
+            this.isEdit = false;
+            // @ts-ignore
+            this.data = this.input.value;
+            this.root.detectChanges();
+        }
+    }
+
+    onChange(): void {
+        if (!this.isEdit) return;
+
+        this.isEdit = false;
+        // @ts-ignore
+        this.data = this.input.value;
+        this.root.detectChanges();
     }
 }
