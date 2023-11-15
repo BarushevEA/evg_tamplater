17653cb5a20bd00c6c688e05266eb4c08342d234
grif <b_evgeny@ukr.net>
2023-11-15 03:33:59 +0200

    update project

--- app/home_organizer/buildOptions/appPath.js
+++ app/home_organizer/buildOptions/appPath.js
@@ -1,7 +1,7 @@
 // Build attributes
 exports.buildDirLevel = "../../../";
-exports.buildDirPath = "./builds/travels";
-exports.buildFileName = "travels.js";
+exports.buildDirPath = "./builds/home_organizer";
+exports.buildFileName = "home_organizer.js";
 
 // App style and templates dirs
 exports.HTMLTemplatesDirPath = "../src/modules/elements/";
