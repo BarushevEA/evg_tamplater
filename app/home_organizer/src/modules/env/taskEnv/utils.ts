0d619cc95da7998c2a3f0dca5462f9701bcb1cc6
grif <b_evgeny@ukr.net>
2023-12-05 00:02:22 +0200

    update id

--- app/home_organizer/src/modules/env/taskEnv/utils.ts
+++ app/home_organizer/src/modules/env/taskEnv/utils.ts
@@ -4,11 +4,11 @@ const symbols3 = ":-.";
 const len1 = symbols1.length - 1;
 const len2 = symbols2.length - 1;
 const len3 = symbols3.length - 1;
-const defaultIdLength = 16;
+const defaultIdLength = 32;
 
-export const getId = (length: number): string => {
+export const getId = (prefix: string, length: number): string => {
     length = Math.round(length / 2);
-    let encrypted = "";
+    let encrypted = prefix;
     encrypted += symbols1[Math.round(Math.random() * len1)] +
         (Math.round(Math.random()) ?
             symbols3[Math.round(Math.random() * len3)] :
@@ -21,7 +21,7 @@ export const getId = (length: number): string => {
                 symbols2[Math.round(Math.random() * len2)]);
     }
 
-    return "#" + encrypted;
+    return encrypted;
 };
 
-export const getDefaultId = (): string => getId(defaultIdLength);
+export const getDefaultId = (): string => getId("id# ", defaultIdLength);
