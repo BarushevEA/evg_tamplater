6b725c85afc8dcc1dbd42c8aac4d3e1e60cb4c0f
grif <b_evgeny@ukr.net>
2024-03-17 19:48:25 +0200

    Add custom directory and error handling functionality to Maker class
    This commit introduces enhanced functionality to the Maker class in the cm.js file by adding the ability to specify custom directory paths for new components. It also incorporates error handling functionality. Additionally, this update refactors import paths and improves overall project code documentation.

--- storage/empty_project_example/cm.js
+++ storage/empty_project_example/cm.js
@@ -1,18 +1,82 @@
 const path = require("path");
 const {getTemplatePath, getModulesPath} = require("./buildTemplateHandler/utils");
 const fs = require("fs");
+/**
+ * Enum representing possible commands.
+ * @enum {string}
+ */
 const COMMAND = {
     CREATE_COMPONENT: "c",
+    COMPONENT_DIR: "d",
 };
 
+/**
+ * Specifies the prefix used for marker strings in the application.
+ *
+ * @type {string}
+ * @constant
+ */
 const markerPrefix = "APP_EXAMPLE_____MARKER_";
+/**
+ * The tagPrefix variable represents the prefix used for custom HTML tags in an application.
+ *
+ * @type {string}
+ */
 const tagPrefix = "app-";
+/**
+ * The path to the template options file.
+ *
+ * @type {string}
+ * @example
+ * const templateOptionsPath = path.join(__dirname, "buildOptions", "templateOptions.js");
+ */
 const templateOptionsPath = path.join(__dirname, "buildOptions", "templateOptions.js");
+/**
+ * Function to get the path of the modules.
+ *
+ * @returns {string} The path of the modules.
+ */
 const modulesPath = getModulesPath();
 
+/**
+ * Represents the command entered as a command line argument.
+ *
+ * @type {string}
+ */
 const cmdCommand = process.argv[2];
+/**
+ * Represents the name of a new component.
+ *
+ * @type {string}
+ */
 const newComponentName = process.argv[3];
 
+/**
+ * Represents the directory path provided as a command line argument.
+ *
+ * @type {string}
+ * @name cmdDir
+ * @description The value of this variable is extracted from the command line arguments list using process.argv[4].
+ *              It should be a string representing the directory path passed as a command line argument.
+ */
+const cmdDir = process.argv[4];
+/**
+ * Represents the command line argument for the new directory.
+ *
+ * @type {string}
+ */
+const newDir = process.argv[5];
+
+/**
+ * Custom directory path.
+ *
+ * @type {string}
+ */
+let customDir = "";
+
+/**
+ * Represents a Maker object which is responsible for creating component templates and registering them in the module.
+ */
 class Maker {
     marker
     tag
@@ -40,7 +104,7 @@ class Maker {
         this.marker = markerPrefix + newComponentName.toUpperCase();
         this.pathPart = newComponentName.toLowerCase();
         this.tag = tagPrefix + this.pathPart;
-        this.dir = this.pathPart;
+        this.dir = path.join(customDir, this.pathPart);
         this.tsFileName = `${this.pathPart}.ts`;
         this.htmlFileName = `${this.pathPart}.html`;
         this.scssMixinFileName = `_${this.pathPart}.scss`;
@@ -52,19 +116,29 @@ class Maker {
         this.absoluteComponentHtmlPath = getTemplatePath(this.componentHtmlPath);
         this.absoluteComponentScssMixinPath = getTemplatePath(this.componentScssMixinPath);
         this.componentClassName = `${this.pathPart[0].toUpperCase()}${this.pathPart.substring(1)}`
-        this.modulesImportString = `import {${this.componentClassName}} from "../modules/elements/${this.pathPart}/${this.pathPart}";`;
+        this.modulesImportString = `import {${this.componentClassName}} from "../modules/elements/${this.dir}/${this.pathPart}";`;
         this.modulesImportStringDecorated = `${this.modulesImportString}\n`;
 
         this.registerOnTemplateOptions();
     }
 
     createTemplates() {
+        const additionalLevel = "../";
+        let levels = "";
+
+        for (let i = 0; i < this.dir.length; i++) {
+            const smb = this.dir[i];
+            if (smb === "/") {
+                levels += additionalLevel;
+            }
+        }
+
         this.htmlTemplate = `<div>Hello ${this.htmlFileName}</div>`;
         this.tsTemplate = `
-import {OnCreate, OnDestroy, OnInit, RootBehavior} from "../../../../../../libs/elements/types";
+import {OnCreate, OnDestroy, OnInit, RootBehavior} from "../../../../../../${levels}libs/elements/types";
 
 export class ${this.componentClassName} implements OnInit, OnCreate, OnDestroy {
-    readonly root: RootBehavior;
+    readonly root;
     name: string;
 
     constructor(root: RootBehavior) {
@@ -133,9 +207,29 @@ export class ${this.componentClassName} implements OnInit, OnCreate, OnDestroy {
 
 handleArguments();
 
+/**
+ * Handles the arguments passed to a command.
+ * If the command is not CREATE_COMPONENT or the newComponentName is not provided,
+ * the function does nothing.
+ * If the command directory is COMPONENT_DIR and a new directory is specified,
+ * the customDir is set to the new directory path.
+ * The function logs information about the command, newComponentName,
+ * modulesPath, templateOptionsPath, variables, and the start and end of the execution.
+ *
+ * @param {string} cmdCommand - The command being executed.
+ * @param {string} newComponentName - The name of the new component being created.
+ * @param {string} cmdDir - The command directory.
+ * @param {string} newDir - The new directory path (optional).
+ * @param {string} modulesPath - The path to the modules.
+ * @param {string} templateOptionsPath - The path to the template options.
+ *
+ * @return {void}
+ */
 function handleArguments() {
     if (cmdCommand !== COMMAND.CREATE_COMPONENT) return;
     if (!newComponentName) return;
+    if (cmdDir === COMMAND.COMPONENT_DIR) customDir = newDir ? newDir : "";
+
     console.log("START");
     console.log();
 
@@ -153,6 +247,13 @@ function handleArguments() {
     console.log("FINISH");
 }
 
+/**
+ * Handles the specified error by throwing it.
+ *
+ * @param {Error} error - The error to be handled.
+ *
+ * @throws {Error} - The error object thrown.
+ */
 function handleError(error) {
     if (error) throw error;
 }
