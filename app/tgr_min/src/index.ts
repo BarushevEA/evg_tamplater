5b62f54d3edb3c3b465a20950311577e503211cc
grif <b_evgeny@ukr.net>
2024-03-23 01:03:14 +0200

    Updated Observable logic and added sorting functionality
    The logic in the various Observable classes has been simplified and streamlined, making it more readable and easier to manage. These updates mainly affect 'get' and 'set' methods for 'isDestroyed', 'isEnable', and 'order'. The most significant addition is a new sorting feature in the "OrderedObservable" class, which can sort Observables in ascending or descending order.

--- app/tgr_min/src/index.ts
+++ app/tgr_min/src/index.ts
@@ -1,8 +1,14 @@
 import {GTimeout} from "./TickGenerator/GTimeout";
 import {GInterval} from "./TickGenerator/GInterval";
 import {GAnimationFrame} from "./TickGenerator/GAnimationFrame";
+import {GTimeoutOrdered} from "./TickGenerator/GTimeoutOrdered";
+import {GIntervalOrdered} from "./TickGenerator/GIntervalOrdered";
+import {GAnimationFrameOrdered} from "./TickGenerator/GAnimationFrameOrdered";
 
 const parent = (<any>window);
 parent.GTimeout = GTimeout;
 parent.GInterval = GInterval;
 parent.GAnimationFrame = GAnimationFrame;
+parent.GTimeoutOrdered = GTimeoutOrdered;
+parent.GIntervalOrdered = GIntervalOrdered;
+parent.qwe = GAnimationFrameOrdered;
