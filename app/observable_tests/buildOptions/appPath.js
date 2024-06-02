a8c70ba5616e3cf1db1a2dddc65be9c90b952d12
grif <b_evgeny@ukr.net>
2024-06-02 19:46:49 +0300

    Add observable_tests for multiple configurations and utilities
    This commit includes observable_tests setups and scripts along with component generation, CSS handling, and template registration. Additions also include Webpack configurations and a script to perform module registrations. CSS and HTML template directories, along with a command handler script, are also included.

---
+++ app/observable_tests/buildOptions/appPath.js
@@ -0,0 +1,11 @@
+// Build attributes
+exports.buildDirLevel = "../../../";
+exports.buildDirPath = "./builds/observable_tests";
+exports.buildFileName = "observable_tests.js";
+
+// App style and templates dirs
+exports.HTMLTemplatesDirPath = "../src/modules/elements/";
+exports.CSSDirPath = "../src/modules/css";
+exports.AssetsDirPath = "../src/modules/css/assets";
+
+exports.ModulesPath = "../src/settings/modules.ts";
---
+++ app/observable_tests/src/settings/info.ts
@@ -0,0 +1,3 @@
+import {AppInfo} from "../../../../libs/utils/appInfo";
+
+export const APP_INFO = new AppInfo('observable_tests', 1, 0, 0);
