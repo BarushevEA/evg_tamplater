9bca172c8b3d92a953608f6bfd0a16cbac6b635e
grif <b_evgeny@ukr.net>
2024-08-11 19:51:44 +0300

    Create project structure for js_text_highlighting
    Added initial project files including configurations for Babel, TypeScript, Webpack, and various other build scripts and utilities. This sets up the basic framework for handling templates, styles, and modules within the js_text_highlighting component.

---
+++ app/js_text_highlighting/buildOptions/appPath.js
@@ -0,0 +1,11 @@
+// Build attributes
+exports.buildDirLevel = "../../../";
+exports.buildDirPath = "./builds/js_text_highlighting";
+exports.buildFileName = "js_text_highlighting.js";
+
+// App style and templates dirs
+exports.HTMLTemplatesDirPath = "../src/modules/elements/";
+exports.CSSDirPath = "../src/modules/css";
+exports.AssetsDirPath = "../src/modules/css/assets";
+
+exports.ModulesPath = "../src/settings/modules.ts";
---
+++ app/js_text_highlighting/src/settings/info.ts
@@ -0,0 +1,3 @@
+import {AppInfo} from "../../../../libs/utils/appInfo";
+
+export const APP_INFO = new AppInfo('js_text_highlighting', 1, 0, 0);
