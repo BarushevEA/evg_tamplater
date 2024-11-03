3cbbeb31db1b996b48b54e93b8b96911001e3d95
grif <b_evgeny@ukr.net>
2024-11-03 19:52:39 +0200

    add routing_demo

---
+++ app/routing_demo/buildOptions/appPath.js
@@ -0,0 +1,11 @@
+// Build attributes
+exports.buildDirLevel = "../../../";
+exports.buildDirPath = "./builds/routing_demo";
+exports.buildFileName = "routing_demo.js";
+
+// App style and templates dirs
+exports.HTMLTemplatesDirPath = "../src/modules/elements/";
+exports.CSSDirPath = "../src/modules/css";
+exports.AssetsDirPath = "../src/modules/css/assets";
+
+exports.ModulesPath = "../src/settings/modules.ts";
---
+++ app/routing_demo/src/settings/info.ts
@@ -0,0 +1,3 @@
+import {AppInfo} from "../../../../libs/utils/appInfo";
+
+export const APP_INFO = new AppInfo('routing_demo', 1, 0, 0);
