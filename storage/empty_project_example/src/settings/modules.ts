4fcc9eb64f8b5d4d4bcedddc093410f7cb07ca6d
grif <b_evgeny@ukr.net>
2024-11-07 20:30:00 +0200

    Add sub-routes registration in empty project example
    Introduce a new `subRoutes.ts` for sub-routes registration and `subRoutesEnums.ts` for sub-route enums. Update `modules.ts` to start sub-routes registration for enhanced routing configuration.

--- storage/empty_project_example/src/settings/modules.ts
+++ storage/empty_project_example/src/settings/modules.ts
@@ -2,9 +2,11 @@ import {getOption, REG_OPTIONS} from "../../../../libs/elements/registrator/regi
 import {AppRoot} from "../modules/elements/appRoot";
 import {APP_TAG_NAME} from "../../../../libs/elements/rootElements/managers/APP_TAG_NAME";
 import {START_ROUTES_REGISTRATION} from "./routes";
+import {START_SUB_ROUTES_REGISTRATION} from "./subRoutes";
 
 export const MODULES: REG_OPTIONS = [
     getOption(AppRoot, APP_TAG_NAME, "APP_EXAMPLE_____ROOT"),
 ];
 
 START_ROUTES_REGISTRATION();
+START_SUB_ROUTES_REGISTRATION();
---
+++ storage/empty_project_example/src/settings/subRoutes.ts
@@ -0,0 +1,10 @@
+import {REGISTER_SUB_ROUTES} from "../../../../libs/elements/rootElements/appSubRout";
+
+export const START_SUB_ROUTES_REGISTRATION = () => true;
+
+REGISTER_SUB_ROUTES(
+
+);
+
+// <qsi-subroute name="SubRoute1"></qsi-subroute>
+// <qsi-subroute name="SubRoute2"></qsi-subroute>
