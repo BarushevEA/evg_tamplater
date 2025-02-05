978423f44e759b40c1ef1da3682427cf0bcf6ba0
grif <b_evgeny@ukr.net>
2025-02-06 01:10:41 +0200

    Add "test_custom-element-subrout" module with build and features
    This commit introduces a new module, "test_custom-element-subrout," including CSS, HTML templates, JavaScript handlers, and routing configurations. It also implements build utilities for asset management, CSS encryption, and template processing needed for component integration and rendering.

---
+++ app/test_custom-element-subrout/buildOptions/templateOptions.js
@@ -0,0 +1,10 @@
+// CSS marker
+exports.CSS_Marker = "APP_EXAMPLE_____STYLE";
+
+// Template markers with they file names
+exports.htmlTemplates = {
+    "APP_EXAMPLE_____ROOT": "root.html",
+    "APP_EXAMPLE_____MARKER_MAIN": "main/main.html",
+    "APP_EXAMPLE_____MARKER_HEADER": "header/header.html",
+    "APP_EXAMPLE_____MARKER_FOOTER": "footer/footer.html",
+};
---
+++ app/test_custom-element-subrout/src/settings/info.ts
@@ -0,0 +1,3 @@
+import {AppInfo} from "../../../../libs/utils/appInfo";
+
+export const APP_INFO = new AppInfo('test_custom-element-subrout', 1, 0, 0);
