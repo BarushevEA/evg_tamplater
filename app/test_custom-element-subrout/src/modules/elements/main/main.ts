32c3fbf65fb298cc71e8e1c34e53cd17459564b5
grif <b_evgeny@ukr.net>
2025-02-06 23:54:29 +0200

    Add button panel styles and subroute message handling
    Introduced a `.buttons_panel` with associated styles to improve UI functionality. Added subroute handling in `AppRoot` for dynamic navigation based on incoming messages (`footer`, `header`, `main`). This enhances structure and interactivity across components.

--- app/test_custom-element-subrout/src/modules/elements/main/main.ts
+++ app/test_custom-element-subrout/src/modules/elements/main/main.ts
@@ -1,15 +1,15 @@
-
-import {OnCreate, OnDestroy, OnInit, RootBehavior, OnMessage} from "../../../../../../libs/env/types";
+import {IChannel, OnCreate, OnDestroy, OnInit, OnMessage, RootBehavior} from "../../../../../../libs/env/types";
 import {log} from "../../../../../../libs/utils/utils";
 
 // Component tag example: <app-main></app-main>
 export class Main implements OnInit, OnCreate, OnDestroy, OnMessage {
     name: string;
+    customElementChannel: IChannel;
 
     constructor(readonly root: RootBehavior) {
         this.name = root.tagName;
     }
-    
+
     onMessage(message: any): void {
         log(this.root.tagName, "message:", message);
     }
@@ -18,8 +18,21 @@ export class Main implements OnInit, OnCreate, OnDestroy, OnMessage {
     }
 
     onInit(): void {
+        this.customElementChannel.sendMessage("Hello from main");
     }
 
     onDestroy(): void {
+    }
+
+    clickMain(): void {
+        this.customElementChannel.sendMessage("main");
+    }
+
+    clickFooter(): void {
+        this.customElementChannel.sendMessage("footer");
+    }
+
+    clickHeader(): void {
+        this.customElementChannel.sendMessage("header");
     }
 }
--- custom_elements/subroute_test/src/modules/elements/appRoot.ts
+++ custom_elements/subroute_test/src/modules/elements/appRoot.ts
@@ -1,4 +1,6 @@
 import {OnCreate, OnDestroy, OnInit, OnMessage, RootBehavior} from "../../../../../libs/env/types";
+import {SUB_ROUTE} from "../../../../../libs/elements/rootElements/appSubRout";
+import {E_SUB_ROUTE, E_SUB_ROUTE_PAGE} from "../../settings/subRoutesEnums";
 
 export class AppRoot implements OnInit, OnCreate, OnDestroy, OnMessage {
     name: string;
@@ -9,6 +11,17 @@ export class AppRoot implements OnInit, OnCreate, OnDestroy, OnMessage {
 
     onMessage(message: any): void {
         console.log(this.root.tagName, "message:", message);
+        switch (message) {
+            case "footer":
+                SUB_ROUTE(E_SUB_ROUTE.TEST).SHOW_PAGE(E_SUB_ROUTE_PAGE.FOOTER);
+                break;
+            case "header":
+                SUB_ROUTE(E_SUB_ROUTE.TEST).SHOW_PAGE(E_SUB_ROUTE_PAGE.HEADER);
+                break;
+            case "main":
+                SUB_ROUTE(E_SUB_ROUTE.TEST).SHOW_PAGE(E_SUB_ROUTE_PAGE.MAIN);
+                break;
+        }
     }
 
     onCreate(): void {
