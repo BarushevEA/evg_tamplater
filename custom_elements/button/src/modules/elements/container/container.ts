3e684793368dbc9e35e1f4f8ceb0e28c3f184079
grif <b_evgeny@ukr.net>
2025-02-20 23:23:39 +0200

    Add container div and bind button options to container
    Wrapped the subroute element with a container div for better structure and extensibility. Introduced `buttonOption` binding logic by subscribing to the `buttonService$` and handling updates dynamically. These changes improve modularity and enable easier customization or future enhancements.

--- custom_elements/button/src/modules/elements/container/container.ts
+++ custom_elements/button/src/modules/elements/container/container.ts
@@ -1,10 +1,15 @@
 
 import {OnCreate, OnDestroy, OnInit, RootBehavior, OnMessage} from "../../../../../../libs/env/types";
 import {log} from "../../../../../../libs/utils/utils";
+import {buttonService$} from "../../services/service";
+import {ButtonOptions} from "../../env/types";
+import {TYPE} from "../../../settings/subRoutesEnums";
 
 // Component tag example: <app-container></app-container>
 export class Container implements OnInit, OnCreate, OnDestroy, OnMessage {
     name: string;
+    container: HTMLElement;
+    buttonOption: ButtonOptions<TYPE>;
 
     constructor(readonly root: RootBehavior) {
         this.name = root.tagName;
@@ -18,8 +23,18 @@ export class Container implements OnInit, OnCreate, OnDestroy, OnMessage {
     }
 
     onInit(): void {
+        this.setButtonOption(buttonService$.getValue());
+        this.root.collect(
+            buttonService$.subscribe(buttonOption => {
+                this.setButtonOption(buttonOption);
+            })
+        );
     }
 
     onDestroy(): void {
+    }
+
+    setButtonOption(buttonOption: ButtonOptions<TYPE>): void {
+        this.buttonOption = buttonOption;
     }
 }
