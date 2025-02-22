5f3c2aaca4b1343aaa0a3a440727c897a8c14794
grif <b_evgeny@ukr.net>
2025-02-22 23:32:39 +0200

    Add test script for custom button functionality
    Introduce a new test script to verify the behavior of the custom button. This ensures the button operates as intended and improves test coverage.

---
+++ app/test_custom_button/buildOptions/appPath.js
@@ -0,0 +1,11 @@
+// Build attributes
+exports.buildDirLevel = "../../../";
+exports.buildDirPath = "./builds/test_custom_button";
+exports.buildFileName = "test_custom_button.js";
+
+// App style and templates dirs
+exports.HTMLTemplatesDirPath = "../src/modules/elements/";
+exports.CSSDirPath = "../src/modules/css";
+exports.AssetsDirPath = "../src/modules/css/assets";
+
+exports.ModulesPath = "../src/settings/modules.ts";
