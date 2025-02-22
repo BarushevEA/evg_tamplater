fd9def69d39da4ba5420e272c3e86b1bb8470598
Sveni2021 <sveni2021@gmail.com>
2025-02-22 22:25:28 +0100

    Add new Custom Element called header. And basic elements.

---
+++ custom_elements/header/buildOptions/templateOptions.js
@@ -0,0 +1,11 @@
+// CSS marker
+exports.CSS_Marker = "APP_EXAMPLE_____STYLE";
+
+// Template markers with they file names
+exports.htmlTemplates = {
+    "APP_EXAMPLE_____ROOT": "root.html",
+    "APP_EXAMPLE_____MARKER_CONTAINER": "container/container.html",
+    "APP_EXAMPLE_____MARKER_LOGO": "container/logo/logo.html",
+    "APP_EXAMPLE_____MARKER_NAVIGATION": "container/navigation/navigation.html",
+    "APP_EXAMPLE_____MARKER_THEMES": "container/themes/themes.html",
+};
---
+++ custom_elements/header/src/settings/modules.ts
@@ -0,0 +1,20 @@
+import {Themes} from "../modules/elements/container/themes/themes";
+import {Navigation} from "../modules/elements/container/navigation/navigation";
+import {Logo} from "../modules/elements/container/logo/logo";
+import {Container} from "../modules/elements/container/container";
+import {getOption, REG_OPTIONS} from "../../../../libs/elements/registrator/registrator";
+import {AppRoot} from "../modules/elements/appRoot";
+import {APP_TAG_NAME} from "../../../../libs/elements/rootElements/managers/APP_TAG_NAME";
+import {START_ROUTES_REGISTRATION} from "./routes";
+import {START_SUB_ROUTES_REGISTRATION} from "./subRoutes";
+
+export const MODULES: REG_OPTIONS = [
+    getOption(AppRoot, APP_TAG_NAME, "APP_EXAMPLE_____ROOT"),
+    getOption(Container, "app-container", "APP_EXAMPLE_____MARKER_CONTAINER"),
+    getOption(Logo, "app-logo", "APP_EXAMPLE_____MARKER_LOGO"),
+    getOption(Navigation, "app-navigation", "APP_EXAMPLE_____MARKER_NAVIGATION"),
+    getOption(Themes, "app-themes", "APP_EXAMPLE_____MARKER_THEMES"),
+];
+
+START_ROUTES_REGISTRATION();
+START_SUB_ROUTES_REGISTRATION();
