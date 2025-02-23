e88a2a1e3bf4683662c29ae38f40a10397b65570
Sveni2021 <sveni2021@gmail.com>
2025-02-23 23:58:43 +0100

    Add Style scheme and default styles for header

---
+++ custom_elements/header/src/modules/env/headerStyles.ts
@@ -0,0 +1,23 @@
+export type ICustomHeaderStyles = {
+    widthHeader: number;
+    heightHeader: number;
+    HeaderColor: string;
+
+    iconWidth: number;
+    iconHeight: number;
+    iconBorderRadius: number;
+
+    buttonWidth: number;
+    buttonHeight: number;
+    buttonFontSize: number;
+
+    logoFontFamily: string;
+    logoFontWeight: string;
+    logoFontColor: string;
+    logoFontSize: number;
+    logoIcon: string;
+    logoText: string;
+
+    themeIcon: string;
+    localization: string[];
+}
