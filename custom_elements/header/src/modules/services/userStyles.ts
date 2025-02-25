89e5de727d93feb4b6363120ad439f0c0e16cb05
Sveni2021 <sveni2021@gmail.com>
2025-02-25 10:49:08 +0100

    add file userStyles

---
+++ custom_elements/header/src/modules/services/userStyles.ts
@@ -0,0 +1,22 @@
+import {ICustomHeaderStyles} from "../env/headerStyles";
+
+export let userStyles: ICustomHeaderStyles = {
+    widthHeader: undefined,
+    heightHeader: undefined,
+    HeaderColor: undefined,
+
+    iconWidth: undefined,
+    iconHeight: undefined,
+    iconBorderRadius: undefined,
+
+    buttonWidth: undefined,
+    buttonHeight: undefined,
+    buttonFontSize: undefined,
+
+    logoFontFamily: undefined,
+    logoFontWeight: undefined,
+    logoFontColor: undefined,
+    logoFontSize: undefined,
+    logoText: undefined,
+    logoIcon: undefined,
+}
