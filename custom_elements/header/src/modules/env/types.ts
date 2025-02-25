a94e5ea7c9e6b304ad955d0b349bf2ed1dcdc58b
Sveni2021 <sveni2021@gmail.com>
2025-02-25 11:02:16 +0100

    modify some elements

---
+++ custom_elements/header/src/modules/env/types.ts
@@ -0,0 +1,21 @@
+export type IHeaderProps = {
+    width?: number;
+    height?: number;
+    color?: string;
+}
+
+export type IIconProps = {
+    img: string;
+    width: number;
+    height: number;
+    borderRadius?: string;
+}
+
+export type ILogoProperties = {
+    fontFamily?: string;
+    fontWeight?: string;
+    fontColor?: string;
+    fontSize?: number;
+    text?: string;
+    icon: string;
+}
